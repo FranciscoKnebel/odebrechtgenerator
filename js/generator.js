b7dbcea9bf248107baeb445f8d55747b5f951202
KyoZeus <franciscopaivaknebel@gmail.com>
2016-03-23 15:00:21 -0300

    Updated goo.gl link

--- js/generator.js
+++ js/generator.js
@@ -18,7 +18,7 @@ app.controller('generatorController', function($scope) {
   $scope.sendTweet = function() {
     var nome = $scope.nome;
     var meunomee = 'Meu apelido da Odebrecht é "'
-    var textToTweet = meunomee.concat(nome, '" - Ache o seu em http://goo.gl/MFpF1x');
+    var textToTweet = meunomee.concat(nome, '" - Ache o seu em http://goo.gl/Nzf2UR');
 
     var twtLink = 'http://twitter.com/home?status=' + encodeURIComponent(textToTweet);
     window.open(twtLink,'_blank');
