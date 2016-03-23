3c7e8155739890cae26cf59f03250de7ba0db74a
KyoZeus <franciscopaivaknebel@gmail.com>
2016-03-23 20:44:44 -0300

    Changed analytics and added more names.

--- js/generator.js
+++ js/generator.js
@@ -5,12 +5,12 @@ app.controller('generatorController', function($scope) {
               "Pastor", "Aspirador", "Escritor", "Metralhadora", "Gatuno", "Mandioca", "Catarro", "Nervosinho", "Correria",
               "Padeiro", "Peteca", "Malote", "Nervosinho", "Havaiana", "Caroço", "Xaninha", "Drácula", "Carniça",
               "Motorista", "Leite com Pêra", "Bingola", "Rio", "Candomblé", "Comuna", "Goleiro", "Bruto", "Colorido", "Roberval",
-			  "Tuca", "Viagra", "Grego", "Atleta", "Neto", "Cacique", "Eva", "Nordeste", "Pelé", "Dadinho", "Zé Pequeno", "Bigodão", "Safadão",
-			  "Capivara", "Fósforo", "Isqueirão", "Máquina", "Dunga", "Mestre dos Magos", "Tijolinho", "Bigorna", "Martelo", "Motoserra",
-			  "Rocambole", "Feijoada", "Bananão", "Cabra", "Surfistinha", "Bola de neve", "Chavoso", "Bola de fogo", "Morango do Nordeste",
-			  "Champinho", "Maracanã", "Lasanha", "Zica", "Aedes Egípcio", "Bolovo", "Vice-Cônsul de Honduras", "Rogérinho",
-			  "Boquetitos"];
-			  
+      			  "Tuca", "Viagra", "Grego", "Atleta", "Neto", "Cacique", "Eva", "Nordeste", "Pelé", "Dadinho", "Zé Pequeno", "Bigodão", "Safadão",
+      			  "Capivara", "Fósforo", "Isqueirão", "Máquina", "Dunga", "Mestre dos Magos", "Tijolinho", "Bigorna", "Martelo", "Motoserra",
+      			  "Rocambole", "Feijoada", "Bananão", "Cabra", "Surfistinha", "Bola de neve", "Chavoso", "Bola de fogo", "Morango do Nordeste",
+      			  "Champinho", "Maracanã", "Lasanha", "Zica", "Aedes Egípcio", "Bolovo", "Vice-Cônsul de Honduras", "Rogérinho",
+      			  "Boquetitos", "Franguinho", "Beija-Flor"];
+
   $scope.nome = jQuery.rand(list);
   $scope.generateNew = function() {
     $scope.nome = jQuery.rand(list);
